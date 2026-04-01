fdace2d32a083652b3013b248bf50f7e1d1f7ad1
Domenico <domenicodeg@gmail.com>
2026-04-01 09:17:29 +0200

    feat: added ai-chatbot interface

--- apps/welcome-frontend/app/(dashboard)/ai-agent-and-chatbot/page.tsx
+++ apps/welcome-frontend/app/(dashboard)/ai-agent-and-chatbot/page.tsx
@@ -22,7 +22,7 @@ export default function AIAgentAndChatbot() {
     {
       role: 'assistant',
       content:
-        "Ciao! Sono l'assistente ROBOT ADHR. Come posso aiutarti oggi? Puoi farmi domande generali o caricarmi un file (CV / Documento) da analizzare.",
+        "Ciao! Sono l'assistente AI-ADHR GDPR FIRST. Come posso aiutarti oggi? Puoi farmi domande generali o caricarmi un file (CV / Documento) da analizzare.",
     },
   ])
   const [input, setInput] = useState('')
