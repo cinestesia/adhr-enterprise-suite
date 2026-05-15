5a68c3be25800925cf58e1fb9d4a354aaebd8dcc
Domenico <domenicodeg@gmail.com>
2026-05-15 16:38:31 +0200

    feat: sessions history

--- apps/welcome-frontend/auth.ts
+++ apps/welcome-frontend/auth.ts
@@ -12,7 +12,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
             //     params: {
             //         scope: "openid profile email phone", // aggiungi qui
             //     }
-            // }
+            // } 
         }),
     ],
     callbacks: {
